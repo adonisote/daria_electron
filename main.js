30f142be3d2143b8934001c06b6ca7dd92c6e64d
adonisote <adonis.almagro@code.berlin>
2024-12-16 16:34:38 +0100

    basic example

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('Hello from electron 👋')
+const { app, BrowserWindow } = require('electron/main')
+
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  })
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
